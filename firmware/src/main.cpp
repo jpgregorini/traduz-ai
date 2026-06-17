beb2973dfd44f434b1b403ab07901dbebb6ac056
Joao Pedro Gregorini <jpgvmoura@outlook.com>
2026-06-17 11:11:34 -0400

    feat(firmware): captura I2S do INMP441 com RMS p/ VAD

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include "config.h"
+#include "audio_capture.h"
+
+static int16_t frame[FRAME_SAMPLES];
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-  Serial.println("[traduzai] firmware iniciado");
+  Serial.println("[traduzai] bring-up do microfone");
+  micBegin();
 }
 
 void loop() {
-  delay(1000);
+  size_t n = micReadFrame(frame);
+  float rms = frameRms(frame, n);
+  Serial.printf("[mic] rms=%.1f\n", rms);
 }
